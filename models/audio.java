cb3a7561d928c581a8f21a8918687bae598a155c
Leonardo Loss <132675221+LeeoLoss@users.noreply.github.com>
2025-07-05 22:35:48 -0300

    Create audio.java

---
+++ models/audio.java
@@ -0,0 +1,35 @@
+package musicapp.models;
+
+public class Audio {
+	private String title;
+	private int totalPlays;
+	private int totalLikes;
+	private int rating;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public int getTotalPlays() {
+		return totalPlays;
+	}
+	public int getTotalLikes() {
+		return totalLikes;
+	}
+	public int getRating() {
+		return rating;
+	}
+	
+	public void like() {
+		this.totalLikes++;
+	}
+	
+	public void play() {
+		this.totalPlays++;
+	}
+	
+}
